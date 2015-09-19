82881c99c2adba2fb2bc573b330fd9e7883b470b
David Barnes <dbarnes@kvcc.edu>
2015-09-19 16:10:34 -0400

    Add some lines to the Main to declare the maze, and starting points. Add variable for instance of MazeSolver, and instanciated one. Add call to MazeSolver method stub SolveMaze.

--- cis237assignment2/Program.cs
+++ cis237assignment2/Program.cs
@@ -8,8 +8,49 @@ namespace cis237assignment2
 {
     class Program
     {
+        /// <summary>
+        /// This is the main entry point for the program.
+        /// You are free to add anything else you would like to this program,
+        /// however the maze solving part needs to occur in the MazeSolver class.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            /// <summary>
+            /// Starting Coordinates.
+            /// </summary>
+            const int X_START = 1;
+            const int Y_START = 1;
+
+            ///<summary>
+            /// The maze that needs to be solved.
+            /// Note: You may want to make a smaller version to test and debug with.
+            /// You don't have to, but it might make your life easier.
+            /// </summary>
+            char[,] maze = 
+            { { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+            { '#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#' },
+            { '#', '.', '#', '.', '#', '.', '#', '#', '#', '#', '.', '#' },
+            { '#', '#', '#', '.', '#', '.', '.', '.', '.', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '#', '#', '#', '.', '#', '.', '.' },
+            { '#', '#', '#', '#', '.', '#', '.', '#', '.', '#', '.', '#' },
+            { '#', '.', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#' },
+            { '#', '#', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#' },
+            { '#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#' },
+            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' } };
+
+            /// <summary>
+            /// Create a new instance of a mazeSolver passing the maze, start coord, and end coord.
+            /// </summary>
+            MazeSolver mazeSolver = new MazeSolver(maze, X_START, Y_START);
+
+            /// <summary>
+            /// Tell the instance to solve the maze
+            /// </summary>
+            mazeSolver.SolveMaze();
+
         }
     }
 }
